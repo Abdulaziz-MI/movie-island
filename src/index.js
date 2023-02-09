7de741795f0eec44d8d1eb2960ca7c17e523db3a
Abdulaziz-MI <AM.Alzaid@outlook.com>
2023-02-09 13:32:08 +0000

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App.js'
-
-// ee9dcaa1
-const container = document.getElementById('container');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
